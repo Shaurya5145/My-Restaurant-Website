22e745834f7056162f448e6f35e986ae41b0276a
SHAURYA GUPTA <gshaurya948@gmail.com>
2025-10-06 21:37:09 +0530

    Update setting.py for production

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -23,11 +23,11 @@ load_dotenv(BASE_DIR / ".env")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["135.235.195.51",""]
 
 
 # Application definition
